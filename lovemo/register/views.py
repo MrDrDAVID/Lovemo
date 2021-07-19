904234eae5fe31da6fe69fa714f79e2ce8c691f2
David <garzadavid26@yahoo.com>
2021-07-19 09:58:42 -0700

    new register app stuff

--- lovemo/register/views.py
+++ lovemo/register/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+def register(request) :
+    return render(request, 'templates/register/register.html')
