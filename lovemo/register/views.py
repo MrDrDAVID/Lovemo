70a9258f650193de3035e3bb2734c8667af957cc
David <garzadavid26@yahoo.com>
2021-07-20 20:19:18 -0700

    registration works now

--- lovemo/register/views.py
+++ lovemo/register/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from .forms import RegisterForm
+from main_app.models import Boo, Lovies
 
 # Create your views here.
 def register(request) :
@@ -9,7 +10,22 @@ def register(request) :
         reg_form = RegisterForm(request.POST)
 
         if reg_form.is_valid() :
-            reg_form.save()
+            new_boo = reg_form.cleaned_data['boo']
+
+            new_user = reg_form.save()
+            new_user.save()
+
+            boo = Boo()
+            boo.user = new_user
+            new_user.boo.boo = new_boo
+            boo.save()
+
+            bank = Lovies()
+            bank.user = new_user
+            new_user.lovies.save()
+
+            return HttpResponseRedirect('/home')
+
     else :
         reg_form = RegisterForm()
 
