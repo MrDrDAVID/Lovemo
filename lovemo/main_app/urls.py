97a758dd4a0ff59f62be80f47b8c3982591f6b1d
David <garzadavid26@yahoo.com>
2021-10-04 15:57:06 -0700

    fixing everything

--- lovemo/main_app/urls.py
+++ lovemo/main_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.index, name='index'),
+    path('', views.index, name='index'),
     path('<int:id>/', views.transaction, name='transaction'),
     path('about/', views.about, name='about'),
     path('creators', views.creators, name='creators'),
